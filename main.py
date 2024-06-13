98db46948a49a79ebe739957e1b6b21f0d53b667
Jerry Velasco <velcojerry@gmail.com>
2024-06-13 10:07:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,30 @@ from tkinter import messagebox
 import random
 import pyperclip
 
+"""searches for passwords in json file"""
+def find_pw():
+    website = website_input.get().capitalize()
+
+    #tries opening file
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+
+    except FileNotFoundError:
+        messagebox.showerror(title="File Doesn't exist", message="No Data File Found.")
+
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email} \nPassword: {password}")
+
+        else:
+            #prompts message if website doesnt exist
+            messagebox.showerror(title="Error", message=f"No details for {website} exist.")
+
+
+
 """creates random password with symbols, letters and numbers"""
 def generate_pw():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -42,24 +66,42 @@ def save_data():
     email_data = email_input.get()
     password_data = password_input.get()
 
+    #creates new dictionary for json file
+    website_dictionary = {
+        website_data: {
+            "email": email_data,
+            "password": password_data
+        }
+    }
+    
     #makes sure inputs arent empty
     if len(website_data) == 0 or len(email_data) == 0 or len(password_data) == 0:
         messagebox.showwarning(title="empty fields", message="Please don't leave any fields empty.")
     else:
-        input_confirmed = messagebox.askokcancel(title=website_data, message=f"These are the details entered: \nEmail: {email_data} \nPassword: {password_data} \nIs it ok to save?")
+        try:
+            with open("data.json", "r") as file:
+                read_data = json.load(file)
+                
+        #creates new json file using inputs if it doesnt exist 
+        except FileNotFoundError:
+            with open("data.json", "w") as file:
+                json.dump(new_data, file, indent=4)
 
-        if input_confirmed:
-            with open("data.txt", "a") as file:
-                file.write(f"{website_data} | {email_data} | {password_data}\n")
+        else:
+            #write new inputs to json file
+            read_data.update(new_data)
+            with open("data.json", "w") as file:
+                json.dump(read_data, file, indent=4) 
 
-                #removes text from fields after writing them to the txt file
-                website_input.delete(first=0, last=END)
-                email_input.delete(first=0, last=END)
-                password_input.delete(first=0, last=END)
-
-                result_label.config(text="Succesfully Added!", fg="green")
-                result_label.after(5000, remove_label)     #removes label after 5secs 
-                website_input.focus()                      #moves cursor to website input box
+        finally:
+            #removes text from fields after writing them to the txt file
+            website_input.delete(first=0, last=END)
+            email_input.delete(first=0, last=END)
+            password_input.delete(first=0, last=END)
+    
+            result_label.config(text="Succesfully Added!", fg="green")
+            result_label.after(5000, remove_label)     #removes label after 5secs 
+            website_input.focus()                      #moves cursor to website input box
 
 
 
@@ -72,6 +114,7 @@ canvas = Canvas(width=200, height=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(column=1, row=0)
+
 
 #labels 
 website_label = Label(text="Website: ")
@@ -86,6 +129,7 @@ password_label.grid(column=0, row=3)
 result_label = Label(text="")
 result_label.grid(column=0, row=4)
 
+
 #inputs 
 website_input = Entry(width=36)
 # website_input.insert(0, string="Enter a website")
@@ -99,7 +143,11 @@ email_input.grid(column=1, row=2, columnspan=2)
 password_input = Entry(width=21)
 password_input.grid(column=1, row=3)
 
+
 #buttons 
+search_button = Button(text="Search", width=11, command=find_pw)
+search_button.grid(column=2, row=1)
+
 generate_pw_button = Button(text="Generate Password", width=11, command=generate_pw)
 generate_pw_button.grid(column=2, row=3)
 
